bc3b98d39c7c9a8007b5fa08104cf933a671c687
etc-ludwig <ludwig.hjarne@gmail.com>
2026-09-11 14:46:46 +0200

    Asks input in while-loop

--- ChessBoard/Program.cs
+++ ChessBoard/Program.cs
@@ -4,11 +4,31 @@
     {
         static void Main(string[] args)
         {
+            bool isInputValid = false;
             string div = "-----------------------------------";
 
             Console.WriteLine(div);
             Console.WriteLine(" Välkommen till vårat Schackbräde!");
             Console.WriteLine(div);
+
+            Console.WriteLine("Bestäm storleken (mellan 3-50) på ditt schackbräde.");
+
+            while (!isInputValid)
+            {   
+                Console.Write("Ange ett tal: ");
+                string input = Console.ReadLine();
+                
+                if (int.TryParse(input, out int size))
+                {
+                    Console.WriteLine("Giltig inmatning!!! JA!!!!!");
+                }
+                else
+                {
+                    Console.WriteLine("Ogiltig inmatning");
+                }
+
+            }
+
         }
     }
 }
