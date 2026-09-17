17d7aa4e1b6bb97c57de3c151254941d98758251
etc-ludwig <ludwig.hjarne@gmail.com>
2026-09-17 11:11:38 +0200

    Moved input logic to GetInput method

--- ChessBoard/Program.cs
+++ ChessBoard/Program.cs
@@ -2,13 +2,13 @@
 {
     class Program
     {
+        public static int size = 0;
+        public static bool isInputValid = false;
+        public static string div = "-----------------------------------";
+
         static void Main(string[] args)
         {
-            // Skriv kommentarer på engelska!!
-
-            bool isInputValid = false;
-            string div = "-----------------------------------";
-            int size = 0;
+            // Skriv kommentarer på engelska
 
             Console.WriteLine(div);
             Console.WriteLine(" Välkommen till vårat Schackbräde!");
@@ -16,33 +16,38 @@
 
             Console.WriteLine("Bestäm storleken (mellan 3-50) på ditt schackbräde.");
 
-            
-            // Sålänge inmatningen är ogiltig, fråga igen
             while (!isInputValid)
-            {   
+            {
                 Console.Write("Ange ett tal: ");
-                
-                // Kollar så att inmatningen är en siffra
-                if (int.TryParse(Console.ReadLine(), out size))
-                {
-                    // Kollar så att det är inom intervallet
-                    if (size >= 3 && size <= 50)
-                    {
-                        isInputValid = true;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Ogiltigt inmatning.");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Inmatningen måste vara ett tal.");
-                }
+                isInputValid = GetInput();
             }
 
             ChessBoard board1 = new ChessBoard();
             board1.RenderBoard(size);
+        }
+
+        public static bool GetInput()
+        {
+            // Kollar så att inmatningen är en siffra
+            if (int.TryParse(Console.ReadLine(), out size))
+            {
+                // Kollar så att det är inom intervallet
+                if (size >= 3 && size <= 50)
+                {
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Ogiltigt inmatning.");
+                    return false;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Inmatningen måste vara ett tal.");
+            }
+
+            return false;
         }
     }
 }
